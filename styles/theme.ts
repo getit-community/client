4a491a1d15a3bdf6b3834c387ce98f3ff1ff8258
seungwonleee <seungwon.code@gmail.com>
2023-02-20 23:00:52 +0900

    Add: theme common styles

---
+++ styles/theme.ts
@@ -0,0 +1,63 @@
+const fontSizes = {
+  xsmall: '1.2rem',
+  small: '1.4rem',
+  base: '1.6rem',
+  large: '1.8rem',
+  xlarge: '2.0rem',
+  xxlarge: '2.2rem',
+  xxxlarge: '2.4rem',
+  titleSize: '3.4rem',
+};
+
+const paddings = {
+  xsmall: '0.5rem',
+  small: '0.8rem',
+  base: '1rem',
+  large: '1.2rem',
+  xlarge: '1.4rem',
+  xxlarge: '1.6rem',
+  xxxlarge: '1.8rem',
+};
+
+const margins = {
+  small: '0.8rem',
+  base: '1rem',
+  large: '1.2rem',
+  xlarge: '1.4rem',
+  xxlarge: '1.6rem',
+  xxxlarge: '1.8rem',
+};
+
+const interval = {
+  small: '2.5rem',
+  base: '5rem',
+  large: '10rem',
+  xlarge: '15',
+  xxlarge: '20rem',
+  xxxlarge: '25rem',
+};
+
+const verticalInterval = {
+  base: `10rem 0 1rem 0`,
+};
+
+const colors = {
+  theme: '#4dd290',
+  black: '#303038',
+  white: '#ffffff',
+  gray: '#767678',
+};
+
+const deviceSizes = {};
+
+const theme = {
+  fontSizes,
+  colors,
+  deviceSizes,
+  paddings,
+  margins,
+  interval,
+  verticalInterval,
+};
+
+export default theme;
